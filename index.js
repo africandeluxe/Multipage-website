c48a5b7e025c4ec9e923ee1c9e8814344d60fecc
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-04-28 12:30:26 +0200

    Added index.js

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ import express from "express";
 const app = express();
 const port = 3002;
 
+app.get('/', (req, res) => {
+  res.send("...")
+});
+
 app.listen(port, console.log(`It's port ${port}`));
